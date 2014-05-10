90144768e240c99c81fa02f075911949a8a749f9
tnietsch <t.nietsch@gmx.de>
2014-05-10 22:15:07 +0200

    Test-001

--- platforms/android/assets/www/js/index.js
+++ platforms/android/assets/www/js/index.js
@@ -2,16 +2,24 @@ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
-        this.testZone = {};
+        this.testzone = {};
     },
 
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener('pause', this.onPause, false);
+        document.addEventListener('resume', this.onResume, false);
     },
 
     onDeviceReady: function() {
         console.log("Ready");
-        app.testZone = document.getElementById("test-zone");
-        app.testZone.innerHTML = "Ready";
-    }
+        app.testzone = document.getElementById("test-zone");
+        app.testzone.innerHTML = "Ready";
+    },
+    onPause: function() {
+        app.testzone.innerHTML += "<br />Paused";
+    },
+    onResume: function() {
+        app.testzone.innerHTML += "<br />Resumed";
+    },
 };
--- platforms/android/gen/de/sparkline_software/V1/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package de.sparkline_software.V1;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
